fc4f239af9a3f8ede12fc5cd4236ba3ed4f2b39f
oknaveen <oknaveen9395@gmail.com>
2019-07-16 11:32:54 -0400

    removed the dummy data and real datas have been added in to database

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-posts=[
-    {
-        'author':'CoreyMs',
-        'title':'Blog Post 1',
-        'content':'First post',
-        'date_posted':'Aug 27th 2019'
-    },
-    {
-        
-        'author':'NaveenMs',
-        'title':'Blog Post 2',
-        'content':'second post',
-        'date_posted':'June 30th 2019'
-    
-    }
-
-
-
-
-]
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
