2c9006e956432be0759381d9a0d99c33c8122492
oknaveen <oknaveen9395@gmail.com>
2019-07-21 02:38:44 -0400

    updating the profile - feature implemented

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email=forms.EmailField()
@@ -10,3 +11,17 @@ class UserRegisterForm(UserCreationForm):
         model=User
         #order of contents in the form
         fields=['username','email','password1','password2']
+
+#updating username and email
+class UserUpdateForm(forms.ModelForm):
+        email=forms.EmailField()
+
+        class Meta:
+            #model to work with
+            model=User
+            fields=['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=['image']
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm , ProfileUpdateForm ,UserUpdateForm
 from django.contrib.auth.decorators import login_required
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 def register(request):
     if request.method=='POST':
@@ -20,4 +24,30 @@ def register(request):
 #user must be logged in to view this page
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    print("hi")
+    #current user
+    if request.method =='POST':
+        print("cjfgfjgdfdfg")
+        #instance=request.user to know which user to update
+        u_form= UserUpdateForm(request.POST,instance=request.user)
+        #new file also comes with the request
+        p_form=ProfileUpdateForm(request.POST,
+                                 request.FILES , 
+                                instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            
+            u_form.save()
+            p_form.save()
+            messages.success(request,f'Your account has been updated!' )
+            return redirect('profile')
+
+
+    else:
+        u_form= UserUpdateForm(instance=request.user)
+        p_form=ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request,'users/profile.html', context)
