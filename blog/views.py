eebd78c8a989a95da1b42d2a5a8b03993974ad34
oknaveen <oknaveen9395@gmail.com>
2019-07-22 10:43:57 -0400

    deleting the post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 # def home(request):
 #     context={
@@ -29,7 +29,7 @@ class PostCreateView(LoginRequiredMixin,CreateView):
         form.instance.author=self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model=Post   
     fields=['title','content']   
 
@@ -37,8 +37,20 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
     def form_valid(self,form):
         form.instance.author=self.request.user
         return super().form_valid(form)    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user==post.author:
+           return True
+        return False 
 
-     
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=Post 
+    success_url='/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user==post.author:
+           return True
+        return False        
    
     
 
