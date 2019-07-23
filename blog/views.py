6af94a2082c2cd45212dc574b3a8994d2c71e233
oknaveen <oknaveen9395@gmail.com>
2019-07-23 11:19:47 -0400

    Update and delete buttons displayed for the logged in user

--- blog/views.py
+++ blog/views.py
@@ -28,6 +28,7 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self,form):
         form.instance.author=self.request.user
         return super().form_valid(form)
+        
 
 class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model=Post   
@@ -39,7 +40,7 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
         return super().form_valid(form)    
     def test_func(self):
         post = self.get_object()
-        if self.request.user==post.author:
+        if self.request.user == post.author:
            return True
         return False 
 
